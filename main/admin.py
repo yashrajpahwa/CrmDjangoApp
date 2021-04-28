baa6702f49f8ee40b07592e584ea36a28ec00a17
Yashraj Pahwa <59451283+yashrajpahwa@users.noreply.github.com>
2021-04-29 00:46:41 +0530

    Delete main directory

--- main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Customer, Product, Order, Tag
-
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(Tag)
